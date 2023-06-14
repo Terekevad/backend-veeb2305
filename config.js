5dfb9c45f9e5a25b3f9ed1b0b287f4d61cfa4b36
Terekevad <130385252+Terekevad@users.noreply.github.com>
2023-06-14 08:56:20 +0300

    backend-veebr2305

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 const { Pool } = require('pg');
-
+const dotenv = require('dotenv');
+ 
+dotenv.config();
+ 
 const pool = new Pool({
-    connectionString: process.env.pg_url,
+    connectionString: 'postgres://karoli:bsxh4atWpksRDt9FnpYaKyrAhs7pD6It@dpg-ci4atl15rnuiierign50-a.frankfurt-postgres.render.com/matkad_za32',
     ssl: {
         rejectUnauthorized: false
     }
 });
+
 module.exports = pool;
